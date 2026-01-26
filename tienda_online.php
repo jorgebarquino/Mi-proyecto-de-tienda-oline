438b0631d4e621d8091c021df9ed5bac86bcb47f
jorgebarquino <jorgebarquino13@gmail.com>
2026-01-26 13:13:58 +0100

    Add files via upload

---
+++ tienda_online.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `tienda_online`
+ */
+
+/* `tienda_online`.`categorias` */
+$categorias = array(
+);
+
+/* `tienda_online`.`detalle_pedido` */
+$detalle_pedido = array(
+);
+
+/* `tienda_online`.`pedidos` */
+$pedidos = array(
+);
+
+/* `tienda_online`.`productos` */
+$productos = array(
+);
+
+/* `tienda_online`.`usuarios` */
+$usuarios = array(
+);
