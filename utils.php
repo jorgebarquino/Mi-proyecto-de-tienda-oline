d2691c9871a6fe3bab8e8617c716f8a1ad1bf537
jorgebarquino <jorgebarquino13@gmail.com>
2026-05-17 18:01:47 +0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -1,29 +1,27 @@
 <?php
+
 function conectar_db(){
-    define("USER_DB","root"); 
-    define("PASSWORD","");
+
+    $host = "sql300.infinityfree.com";
+    $dbname = "if0_41917398_tienda";
+    $user = "if0_41917398";
+    $password = "VUxueXhauceah9";
+
     try {
-        $dsn = "mysql:host=localhost;dbname=tienda_online";
-        $con = new PDO($dsn, USER_DB, PASSWORD);
+
+        $dsn = "mysql:host=$host;dbname=$dbname";
+
+        $con = new PDO($dsn, $user, $password);
+
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         return $con;
+
     } catch (PDOException $e){
-        echo 'Error: '.$e->getMessage();
+
+        die("Error de conexión: " . $e->getMessage());
+
     }
 }
 
-function getParams($input) {
-    $filterParams = [];
-    foreach($input as $param => $value) {
-        $filterParams[] = "$param=:$param";
-    }
-    return implode(", ", $filterParams);
-}
-
-function bindAllValues($statement, $params) {
-    foreach($params as $param => $value) {
-        $statement->bindValue(':'.$param, $value);
-    }
-    return $statement;
-}
 ?>
